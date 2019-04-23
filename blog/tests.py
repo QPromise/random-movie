236563bffaabfe28909411b89452b254786cb0fa
QPromise <815221919@qq.com>
2019-04-23 22:23:57 +0800

    重新提交settings.py

--- blog/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-from django.core.paginator import Paginator
-item_list = ['john', 'paul', 'george', 'ringo']
-p = Paginator(item_list, 3)
-page2 = p.page(1)
-print(page2.object_list)
